17018d02559f2c140ff682021e97235ed09deb38
acallahan97 <callahanamanda877@gmail.com>
2021-12-13 13:27:08 -0700

    initial commit

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarLotSimulator
+{
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+        public bool IsDriveable { get; set; }
+
+        public string MakeEngineNoise()
+        {
+            if (IsDriveable = false)
+            {
+                return "quiet";
+            }
+            else
+            {
+                return "loud";
+            }
+        }
+        public string MakeHonkNoise()
+        {
+            if (IsDriveable = true)
+            {
+                return "honk";
+            }
+            else
+            {
+                return "no honk";
+            }
+        }
+    }
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -17,6 +17,41 @@ namespace CarLotSimulator
             //Now that the Car class is created we can instanciate 3 new cars
             //Set the properties for each of the cars
             //Call each of the methods for each car
+            Car honda = new Car() { Year = 1998,
+                Make = "Honda",
+                Model = "Civic", 
+                EngineNoise = "loud", 
+                HonkNoise = "quiet", 
+                IsDriveable = true };
+            Console.WriteLine($"Make: {honda.Make}, Model: {honda.Model}, " +
+                $"Year: {honda.Year}, Engine Noise Level: {honda.EngineNoise}," +
+                $"Horn Noise Level: {honda.HonkNoise}, Runs and Drives: {honda.IsDriveable}");
+
+            Car ford = new Car()
+            {
+                Year = 2021,
+                Make = "Ford",
+                Model = "F-150",
+                EngineNoise = "loud",
+                HonkNoise = "quiet",
+                IsDriveable = false
+            };
+            Console.WriteLine($"Make: {ford.Make}, Model: {ford.Model}, " +
+                $"Year: {ford.Year}, Engine Noise Level: {ford.EngineNoise}," +
+                $"Horn Noise Level: {ford.HonkNoise}, Runs and Drives: {ford.IsDriveable}");
+
+            Car toyota = new Car()
+            {
+                Year = 2022,
+                Make = "Toyota",
+                Model = "Highlander",
+                EngineNoise = "quiet",
+                HonkNoise = "loud",
+                IsDriveable = true
+            };
+            Console.WriteLine($"Make: {toyota.Make}, Model: {toyota.Model}, " +
+                $"Year: {toyota.Year}, Engine Noise Level: {toyota.EngineNoise}," +
+                $"Horn Noise Level: {toyota.HonkNoise}, Runs and Drives: {toyota.IsDriveable}");
 
             //*************BONUS*************//
 
